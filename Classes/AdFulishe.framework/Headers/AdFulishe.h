9a6a74f4396cc90e0044d8fce8da715ab729d5e0
Zhang Weifan <374604443@163.com>
2021-02-23 17:51:00 +0800

    修改framework

---
+++ Classes/AdFulishe.framework/Headers/AdFulishe.h
@@ -0,0 +1,20 @@
+//
+//  AdFulishe.h
+//  AdFulishe
+//
+//  Created by 张维凡 on 2020/11/18.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for AdFulishe.
+FOUNDATION_EXPORT double AdFulisheVersionNumber;
+
+//! Project version string for AdFulishe.
+FOUNDATION_EXPORT const unsigned char AdFulisheVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AdFulishe/PublicHeader.h>
+
+#import "HHAdViewManager.h"
+#import <GDTAd/GDTSDKConfig.h>
+#import <BUAdSDK/BUAdSDK.h>
